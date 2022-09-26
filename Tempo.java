6cc2ace7eaeca0cef3951a23c357d5d056ee1a7c
Tiago Da Silva <64613279+tiagojesilva@users.noreply.github.com>
2022-09-26 09:13:35 -0300

    classe tempo
    essa classe está no inicio tempo

---
+++ Tempo.java
@@ -0,0 +1,34 @@
+
+/**
+ * Lista 3, ques 2
+ * Essa classe representa o momento.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class Tempo
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int hora;
+    private int minuto;
+    private int segundo;
+
+    /**
+     * Construtor para objetos da classe Tempo
+     */
+    public Tempo(int hora, int minuto, int segundo)
+    {
+        // inicializa variáveis de instância
+        this.hora = hora;
+        this.minuto = minuto;
+        this.segundo = segundo;
+    }
+
+    /**
+     * Um exemplo de um método - substitua este comentário pelo seu próprio
+     * 
+     * @param  y   um exemplo de um parâmetro de método
+     * @return     a soma de x e y 
+     */
+     
+}
